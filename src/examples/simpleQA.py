0cf84bf6c2c0d4b7e0c2f6b0f7daa67fb1b8c4d5
cehl-kurage <yusakuishihira@gmail.com>
2023-03-28 11:44:21 +0900

    ADD simple example

---
+++ src/examples/simpleQA.py
@@ -0,0 +1,15 @@
+from langchain import PromptTemplate, OpenAI, LLMChain
+
+API_KEY = "YOUR API KEY"
+template = """質問：{question}
+回答：段階的に考えましょう。
+"""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(
+    prompt=prompt, llm=OpenAI(temperature=0, openai_api_key=API_KEY), verbose=True
+)
+
+question = "関ヶ原の戦いはいつ起きた？"
+
+answer = llm_chain.predict(question=question)
+print(answer)
