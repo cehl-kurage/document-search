c1fc23baa5d7c7423d50b03473c90414affc96f1
cehl-kurage <yusakuishihira@gmail.com>
2023-05-30 16:24:45 +0900

    comment out scripts about file

--- src/main.py
+++ src/main.py
@@ -5,18 +5,23 @@ from omegaconf import DictConfig
 
 from models.Searcher import Searcher
 
-pyrootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+root = pyrootutils.setup_root(
+    __file__, indicator=".project-root", pythonpath=True
+)  # search_from __file__
 
 
-@hydra.main(config_path="../configs", config_name="config.yaml")
+@hydra.main(version_base="1.1", config_path="../configs", config_name="config.yaml")
 def run_gradio(cfg: DictConfig):
     api_key_input = gr.inputs.Textbox(label="API KEY")
     query_input = gr.inputs.Textbox(lines=3, label="query")
+    # file_input = gr.File()
     result_output = gr.outputs.Textbox(label="Search result")
     searcher = Searcher(cfg.model)
 
     interface = gr.Interface(
-        fn=searcher.search, inputs=[api_key_input, query_input], outputs=result_output
+        fn=searcher.search,
+        inputs=[api_key_input, query_input],
+        outputs=result_output,
     )
 
     interface.launch(share=True)
